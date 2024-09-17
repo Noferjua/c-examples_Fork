fa6aa5f0a546f12bd9bf4f68e4c860bde98facf0
Noelia <noelia.fernandezjuarez@ucr.ac.cr>
2024-09-17 16:11:46 -0600

    added funcion5() in feature-branch

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -9,6 +9,7 @@ int main() {
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
+    funcion5(10);
 
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
@@ -36,9 +37,9 @@ int funcion3(int x) {
     return y;
 }
 
+
 int funcion5(int x) {
-    printf("en funcion5");
-int funcion4(int x) {
-    printf("En funcion4()");
-    return x;
+    printf("En funcion5()...\n");
+    int y = x + 5;
+    return y;
 }
